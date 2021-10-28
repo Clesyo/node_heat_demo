89aa7afbe0bc9dfb38691ea7a632299903ae5436
clesyo.silva <clesyo.silva@maidahealth.com>
2021-10-27 21:43:14 -0300

    Criando mensagem, criando comunicação socket.io, fazendo busca de mensagem e usuarios

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controller/AuthenticateUserController";
+import { CreateMessageController } from "./controller/CreateMessageController";
+import { GetLast3MessageController } from "./controller/GetLast3MessageController";
+import { ProfileUserController } from "./controller/ProfileUserController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle)
+
+router.post("/message", ensureAuthenticated, new CreateMessageController().handle);
+
+router.get("/messages/last3", new GetLast3MessageController().handle);
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
 
 export {router}
